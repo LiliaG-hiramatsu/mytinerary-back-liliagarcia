7c3a06d7eeddc68c5dc27ca4925a07a26685f495
Lilia García Hiramatsu <liliagarciahiramatsu@gmail.com>
2023-09-11 12:55:51 -0300

    validation joi done! (#10)

--- schemas/signin.js
+++ schemas/signin.js
@@ -1,17 +1,18 @@
 import joi from "joi";
 
 let signinSchema = joi.object({
-    mail: joi.string().required().min(8).max(20).messages({
+    mail: joi.string().required().min(8).max(20).email({ minDomainSegments: 2}).messages({
         'string.min': "mail must have at least 8 characters!",
         'string.max': "mail must be less than 20 characters!",
         'any.required': "mail is required", //para cuando NO se envia el dato
-        'string.empty': "mail cannot be empty!" //para cuando se envia VACIO
+        'string.empty': "mail cannot be empty!", //para cuando se envia VACIO
+        'string.email': 'invalid email'
     }),
     password: joi.string().required().min(8).max(20).messages({
         'string.min': "password must have at least 8 characters!",
         'string.max': "password must be less than 20 characters!",
-        'any.required': "password is required", //para cuando NO se envia el dato
-        'string.empty': "password cannot be empty!" //para cuando se envia VACIO
+        'any.required': "password is required",
+        'string.empty': "password cannot be empty!"
     })
 })
 
